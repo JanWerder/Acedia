3bf132b7f5b4a5e78676f2e791c7acea215fd91f
tietze111 <tietze111@yahoo.de>
2013-11-21 17:11:19 +0100

    ok, well, the addforce is executed every time and the debug log in the same function only the first time... damn

--- Assets/Scripts/Tiles/JumppadBehaviour.cs
+++ Assets/Scripts/Tiles/JumppadBehaviour.cs
@@ -15,6 +15,7 @@ public class JumppadBehaviour : MonoBehaviour {
 		if(Vector3.Distance(this.gameObject.transform.position, goPlayer.transform.position) <= 0.5f){
 			goPlayer.transform.rigidbody.velocity = Vector3.zero;
 			goPlayer.transform.rigidbody.AddForce(Vector3.up * 23000000);
+			Debug.Log(goPlayer.transform.rigidbody.velocity.y + "!");
 		}
 	}
 }
