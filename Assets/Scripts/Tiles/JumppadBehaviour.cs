8f2aa1c4c9c3af816764e20354d91e9119638c93
Jan <jackflash5500@gmail.com>
2013-11-21 19:02:11 +0100

    Reworked Jump Code (#1)
    - lowered the distance needed, which leads to more similar jumps (first
    vs second)
    - adjusted the force applied
    - added a simple timer, so the force couldn't culminate

--- Assets/Scripts/Tiles/JumppadBehaviour.cs
+++ Assets/Scripts/Tiles/JumppadBehaviour.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class JumppadBehaviour : MonoBehaviour {
 
 	private GameObject goPlayer;
+	private float timer;
 
 	void Start () {
 		goPlayer = SceneData.GetInstance().goPlayer;
-	
+		timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Vector3.Distance(this.gameObject.transform.position, goPlayer.transform.position) <= 0.5f){
+		timer += Time.deltaTime;
+		if(Vector3.Distance(this.gameObject.transform.position, goPlayer.transform.position) <= 0.2 && timer > 1.0f){
+			timer = 0;
 			goPlayer.transform.rigidbody.velocity = Vector3.zero;
-			goPlayer.transform.rigidbody.AddForce(Vector3.up * 23000000);
-			Debug.Log(goPlayer.transform.rigidbody.velocity.y + "!");
+			goPlayer.transform.rigidbody.AddForce(Vector3.up * 30000000);
+
 		}
 	}
 }
